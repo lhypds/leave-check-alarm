79e318f7507f0bd85eda59ce781caddd343238e0
Heyang Liu <lhypds@gmail.com>
2020-11-01 18:40:18 +0900

    Update .gitignore

--- LeaveCheckAlarm/LeaveCheckAlarmApp.swift
+++ LeaveCheckAlarm/LeaveCheckAlarmApp.swift
@@ -18,3 +18,9 @@ struct LeaveCheckAlarmApp: App {
         }
     }
 }
+
+struct LeaveCheckAlarmApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
